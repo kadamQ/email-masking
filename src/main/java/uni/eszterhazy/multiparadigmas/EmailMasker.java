a54bd05bbec85310297e37b2dd3d9dedaaafae7e
kadamQ <kovadam95@gmail.com>
2020-12-18 13:58:05 +0100

    Email masking added

--- src/main/java/uni/eszterhazy/multiparadigmas/EmailMasker.java
+++ src/main/java/uni/eszterhazy/multiparadigmas/EmailMasker.java
@@ -4,11 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailMasker {
+    private static final int LENGTH_BELOW_FIRST_CHARACTER_MASKING = 4;
 
-    public boolean validateEmail(String email)
-    {
+    protected boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(email);
         return matcher.find();
+    }
+
+    public String maskEmail(String email) {
+        if (!validateEmail(email))
+            throw new IllegalArgumentException("Invalid email format!");
+        String domain = email.substring(email.indexOf('@'));
+        String textBeforeAt = email.substring(0, email.indexOf('@'));
+        if(textBeforeAt.length() < LENGTH_BELOW_FIRST_CHARACTER_MASKING)
+            return textBeforeAt.replaceAll("(?<!^).", "*") + domain;
+        else
+            return textBeforeAt.replaceAll("(?<!^).(?!$)", "*") + domain;
     }
 }
--- src/test/java/uni/eszterhazy/multiparadigmas/EmailMaskerTest.java
+++ src/test/java/uni/eszterhazy/multiparadigmas/EmailMaskerTest.java
@@ -7,24 +7,56 @@ import org.junit.jupiter.api.Test;
 class EmailMaskerTest {
 
     EmailMasker emailMasker;
+
     @BeforeEach
     void setUp() {
-         emailMasker = new EmailMasker();
+        emailMasker = new EmailMasker();
     }
 
     @Test
-    public void  testIsEmailValid(){
+    public void testIsEmailValid() {
         boolean actual = emailMasker.validateEmail("kovadam95@gmail.com");
-        Assertions.assertEquals(true,actual);
+        Assertions.assertTrue(actual);
     }
+
     @Test
-    public void testIsEmailInvalidMissingAt(){
+    public void testIsEmailInvalidMissingAt() {
         boolean actual = emailMasker.validateEmail("kovadam95gmail.com");
-        Assertions.assertEquals(false,actual);
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testIsEmailInvalidMissingDomainName() {
+        boolean actual = emailMasker.validateEmail("kovadam95@");
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testMasking() {
+        String actual = emailMasker.maskEmail("kovadam95@gmail.com");
+        String expected = "k*******5@gmail.com";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMaskingUnder4CharactersLength() {
+        String actual = emailMasker.maskEmail("sor@gmail.com");
+        String expected = "s**@gmail.com";
+        Assertions.assertEquals(expected, actual);
     }
     @Test
-    public void testIsEmailInvalidMissingDomainName(){
-        boolean actual = emailMasker.validateEmail("kovadam95@");
-        Assertions.assertEquals(false,actual);
+    public void testMaskingOnly1CharacterLength() {
+        String actual = emailMasker.maskEmail("s@gmail.com");
+        String expected = "s@gmail.com";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCheckInvalidCharacter() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> emailMasker.maskEmail("sör@gmail.com"));
+    }
+    @Test
+    public void testCheckInvalidSpecialCharacter() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> emailMasker.maskEmail("s&r@gmail.com"));
     }
 }
